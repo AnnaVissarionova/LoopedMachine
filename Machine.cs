389bd6803032716b7d421fa3c95e27b093331ccc
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-04-21 17:06:42 +0300

    final

--- Machine.cs
+++ Machine.cs
@@ -12,13 +12,41 @@ namespace LoopedMachine
         Dictionary<int, Dictionary<char, int[]>> q_table;
         int length;
         int steps_count;
-        public Machine(int[] conds, string[] transf, int l)
+        public Machine(int conds, string[] transf, int l)
         {
-            q_table = DefineConditionsDictionary(conds, transf);
+            q_table = DefineConditionsDictionary(CreateArr(conds), transf);
             comb = CreateCombDict(q_table);
             length = l;
             steps_count = 0;
         }
+
+
+        //создает словарь со строками-описаниями машины для каждого возможного состояния 
+        static Dictionary<int, Dictionary<char, int[]>> DefineConditionsDictionary(int[] conds, string[] transforms)
+        {
+            var dict = new Dictionary<int, Dictionary<char, int[]>>();
+            for (var i = 0; i < conds.Length; i++)
+            {
+                string[] cur_tf = transforms.Where(x => x.StartsWith($"q{i + 1}")).ToArray();
+                var inner_dict = new Dictionary<char, int[]>();
+                foreach (var c in cur_tf)
+                {
+                    var ss = c.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    var t = "";
+                    foreach (var h in ss[3..])
+                    {
+                        t = t + " " + h;
+                    }
+                    inner_dict.Add(ss[1][0], ParseTransormation(t));
+                }
+                dict.Add(conds[i], inner_dict);
+
+            }
+            return dict;
+        }
+
+
+        //создает словарь со всеми строками-описаниями работы машины и количеством их повторений
         static Dictionary<int[], int> CreateCombDict(Dictionary<int, Dictionary<char, int[]>> q_table)
         {
             var dict = new Dictionary<int[], int>();
@@ -33,18 +61,8 @@ namespace LoopedMachine
             return dict;
         }
 
-        public void PrintComb()
-        {
-            foreach (var a in comb)
-            {
-                foreach (var e in a.Key)
-                {
-                    Console.Write($"{e} ");
-                }
-                Console.WriteLine(a.Value);
-            }
-        }
-
+       
+        //проверяет повтор комбинации
         bool CheckCobminations(Dictionary<int[], int> comb)
         {
             foreach (var i in comb)
@@ -57,18 +75,114 @@ namespace LoopedMachine
             return true;
         }
 
+        //возвращает строку-описание в виде массива [входное_состояние, символ, выходное_состояние, новый_символ, смещение]
         static int[] GetTransformInArr(Dictionary<int, Dictionary<char, int[]>> q_table, int q, char c)
         {
             var res = new int[5];
             res[0] = q;
             var dict = q_table.GetValueOrDefault(q);
-            //res[1] = c == 'a' ? 0 : 1;
             res[1] = c;
             res[2] = dict.GetValueOrDefault(c)[0];
             res[3] = dict.GetValueOrDefault(c)[1];
             res[4] = dict.GetValueOrDefault(c)[2];
             return res;
         }
+
+       
+        
+        public (int, int, string) OneStep(int q, string text, int cur_pos)
+        {
+            text = CheckText(text);
+            (int next_cond, int next_pos) = (-1, -1);
+            var cur_char = text[cur_pos];
+            var dict = q_table.GetValueOrDefault(q);
+            if (dict.GetValueOrDefault(cur_char) == null)
+            {
+                return (-1, -1, text); //сигнал об окончании работы
+            }
+            text = text[..cur_pos] + (char)dict.GetValueOrDefault(cur_char)[1] + text[(cur_pos + 1)..];
+            next_cond = dict.GetValueOrDefault(cur_char)[0];
+            next_pos = cur_pos + dict.GetValueOrDefault(cur_char)[2];
+
+
+            if (next_pos == -1) { next_pos = length - 1; }
+            if (next_pos == length) { next_pos = 0; }
+
+            steps_count++;
+            return (next_cond, next_pos, text);
+        }
+
+        //проверяет на наличие комбинации, которая зацикливает машину на одном месте 
+        //пример: q1 a -> q1 a 0
+        (bool, string) CheckForCycleInOnePosition()
+        {
+            foreach(var q in q_table)
+            {
+                foreach(var i in q.Value)
+                {
+                   // Console.WriteLine($"{q.Key} == {i.Value[0]} && {i.Key} == {i.Value[1]} && {i.Value[2]} == 0");
+                    if(q.Key == i.Value[0] && i.Key == (char) i.Value[1] && i.Value[2] == 0)
+                    {
+                        return (true, "q" + q.Key + " " + i.Key + " -> q" + i.Value[0] + " " + (char) i.Value[1] + " " + i.Value[2]);
+                    }
+                }
+            }
+            return (false,"");
+        }
+
+        public void RunMachine(int q, string text, int cur_pos)
+        {
+            text = CheckText(text);
+            if (CheckForCycleInOnePosition().Item1)
+            {
+                Console.WriteLine($"------> Машина содержит зацикливающую комбинацию {CheckForCycleInOnePosition().Item2}");
+            }
+            else
+            {
+                while (q != -1)
+                {
+                    if (CheckCobminations(comb) && steps_count > length)
+                    {
+                        Console.WriteLine("------> Машина прошла все комбинации и зациклилась");
+                        break;
+                    }
+                    (int q_prev, int pos_prev, char prev) = (q, cur_pos, text[cur_pos]);
+                    (q, cur_pos, text) = OneStep(q, text, cur_pos);
+                    if (q != -1)
+                    {
+                        UpdateComb(q_prev, prev);
+                    }
+                }
+                if (q == -1) Console.WriteLine("------> Машина не зациклилась");
+            }
+            
+        }
+
+        //обновляет количество выполненных комбинаций
+         void UpdateComb(int q, char c)
+        {
+            var arr = new int[5];
+            arr[0] = q;
+            arr[1] = c;
+            var dict = q_table.GetValueOrDefault(q);
+            arr[2] = dict.GetValueOrDefault(c)[0];
+            arr[3] = dict.GetValueOrDefault(c)[1];
+            arr[4] = dict.GetValueOrDefault(c)[2];
+           
+            var kvarr = comb.Keys;
+            for(var i =0; i < kvarr.Count; i++)
+            {
+                if (CheckArrs(kvarr.ElementAt(i), arr))
+                {
+                    var s = comb.GetValueOrDefault(kvarr.ElementAt(i));
+                    comb[kvarr.ElementAt(i)] = s + 1;
+                }
+            }
+         
+        }
+
+
+
 
         string CheckText(string text)
         {
@@ -79,142 +193,16 @@ namespace LoopedMachine
             return text;
         }
 
-        static Dictionary<int, Dictionary<char, int[]>> DefineConditionsDictionary(int[] conds, string[] transforms)
+        static int[] CreateArr(int n)
         {
-            var dict = new Dictionary<int, Dictionary<char, int[]>>();
-            for (var i = 0; i < conds.Length; i++)
+            var res = new int[n];
+            for (var i = 0; i < n; i++)
             {
-                string[] cur_tf = transforms.Where(x => x.StartsWith($"q{i + 1}")).ToArray();
-                var inner_dict = new Dictionary<char, int[]>();
-                foreach (var c in cur_tf)
-                {
-                    var ss = c.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-                    // Console.WriteLine(ss[2]);
-                    var t = "";
-                    foreach (var h in ss[3..])
-                    {
-                        t = t + " " + h;
-                    }
-                    Console.WriteLine(ss[1][0]);
-                    inner_dict.Add(ss[1][0], ParseTransormation(t));
-                }
-                dict.Add(conds[i], inner_dict);
-
-                //dict.Add(conds[i], inner_dict[i]);
+                res[i] = i + 1;
             }
-            return dict;
+            return res;
         }
 
-        public (int, int, string) OneStep(int q, string text, int cur_pos)
-        {
-            text = CheckText(text);
-            (int next_cond, int next_pos) = (-1, -1);
-            var cur_char = text[cur_pos];
-            var dict = q_table.GetValueOrDefault(q);
-            if (dict.GetValueOrDefault(cur_char) == null)
-            {
-                Console.WriteLine("step is not made");
-                return (-1, -1, text); //сигнал об окончании работы
-            }
-            //text = text[..cur_pos] + (dict.GetValueOrDefault(cur_char)[1] == 0 ? 'a' : 'b') + text[(cur_pos+1)..];
-            text = text[..cur_pos] + (char)dict.GetValueOrDefault(cur_char)[1] + text[(cur_pos + 1)..];
-            next_cond = dict.GetValueOrDefault(cur_char)[0];
-            next_pos = cur_pos + dict.GetValueOrDefault(cur_char)[2];
-
-            Console.WriteLine("step is made");
-
-            if (next_pos == -1) { next_pos = length - 1; }
-            if (next_pos == length) { next_pos = 0; }
-
-            steps_count++;
-            return (next_cond, next_pos, text);
-        }
-
-
-        public (int, int, string) MakeNSteps(int q, string text, int cur_pos, int steps)
-        {
-            text = CheckText(text);
-            for (var i = 0; i < steps; i++)
-            {
-                (q, cur_pos, text) = OneStep(q, text, cur_pos);
-                if(q == -1)
-                {
-                    break;
-                }
-            }
-            return (q, cur_pos, text);
-        }
-
-        public void RunMachine(int q, string text, int cur_pos)
-        {
-            text = CheckText(text);
-            while (q != -1)
-            {
-                if (CheckCobminations(comb) && steps_count > length)
-                {
-                    Console.WriteLine("машина прошла все комбинации и зациклилась");
-                    break;
-                }
-                foreach (var c in comb)
-                {
-                    PrintComb(c);
-                }
-                (int q_prev, int pos_prev, char prev) = (q, cur_pos, text[cur_pos]);
-                (q, cur_pos, text) = OneStep(q, text, cur_pos);
-                if (q != -1)
-                {
-                    UpdateComb(q_prev, prev);
-                }
-                Console.Write($"text : {text} ; ");
-                Console.Write($"cond : {q}; ");
-                Console.WriteLine($"pos : {cur_pos}");
-            }
-           if (q == -1)  Console.WriteLine("машина не зациклилась");
-        }
-
-       
-
-        static void PrintComb(KeyValuePair<int[], int> t)
-        {
-            Console.WriteLine($"q{t.Key[0]} {t.Key[1]} -> q{t.Key[2]} {t.Key[3]} {t.Key[4]} sum = {t.Value}");
-        }
-
-         void UpdateComb(int q, char c)
-        {
-            Console.WriteLine("starting update");
-            var arr = new int[5];
-            arr[0] = q;
-            arr[1] = c;
-            var dict = q_table.GetValueOrDefault(q);
-            arr[2] = dict.GetValueOrDefault(c)[0];
-            arr[3] = dict.GetValueOrDefault(c)[1];
-            arr[4] = dict.GetValueOrDefault(c)[2];
-            /*foreach (var i in comb)
-            {
-                if (CheckArrs(i.Key, arr))
-                {
-                    var s = i.Value;
-                    comb.Remove(i.Key);
-                    Console.WriteLine("updates1");
-                    comb.Add(i.Key, s + 1);
-                }
-            }*/
-            var kvarr = comb.Keys;
-            for(var i =0; i < kvarr.Count; i++)
-            {
-                if (CheckArrs(kvarr.ElementAt(i), arr))
-                {
-                    var s = comb.GetValueOrDefault(kvarr.ElementAt(i));
-                    //Console.WriteLine("removeing " + comb.Remove(kvarr.ElementAt(i)));
-                    Console.WriteLine("updates1");
-                    comb[kvarr.ElementAt(i)] = s + 1;
-                }
-            }
-              
-            
-        }
-
-        
 
         static bool CheckArrs(int[] a, int[]b)
         {
@@ -235,15 +223,12 @@ namespace LoopedMachine
         static int[] ParseTransormation(string tf)
         {
             var res = new int[3];
-            // Console.WriteLine(tf);
             var ss = tf.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-            // Console.WriteLine(ss[0]);
-            //Console.WriteLine(tf);
             res[0] = int.Parse(ss[0].Substring(1));
-            // res[1] = ss[1].Equals("a") ? 0 : 1;
             res[1] = ss[1][0];
-            res[2] = ss[2].Equals("+1") ? 1 : -1;
+            res[2] = ss[2].Contains('+') ? 1 : int.Parse(ss[2]);
             return res;
         }
+
     }
 }
--- main.cs
+++ main.cs
@@ -12,7 +12,51 @@ namespace LoopedMachine
         
         static void Main(string[] args)
         {
-            var tf = new string[]
+
+            var q = 0;
+            var m = 0;
+            var tf = new string[0];
+            var text = "";
+            var length = 0;
+
+            var inp = "";
+
+            Console.Write("Введите количество состояний машины (exit для выхода) : ");
+            inp = Console.ReadLine();
+            while (!inp.Equals("exit"))
+            {
+                
+                q = int.Parse(inp);
+                Console.Write("Введите количество строк с описанием машины : ");
+                m = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите строки с описанием машины : ");
+
+                for (var i = 0; i < m; i++ )
+                {
+                    tf = tf.Append(Console.ReadLine()).ToArray();
+                }
+
+                Console.WriteLine("Введите входные данные на ленте (без пробелов) : ");
+                text = Console.ReadLine();
+
+                Console.Write("Введите длину ленты : ");
+                length = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("~~~~~~~ Запускаем проверку машины");
+                var mch = new Machine(q, tf, length);
+
+                (int nc, int np) = (1, 0);
+
+                mch.RunMachine(nc, text, np);
+
+                Console.WriteLine();
+
+                Console.Write("Введите количество состояний машины (exit для выхода) : ");
+                inp = Console.ReadLine();
+            }
+
+            tf = new string[]
         {
             "q1 a  -> q1 b +1",
             "q1 b  -> q1 a +1",
@@ -24,53 +68,9 @@ namespace LoopedMachine
 
         };
 
-            var mch = new Machine(new int[] { 1, 2 }, tf, 5);
-            /*var dict = DefineCOnditionsDictionary(new int[] { 1, 2, 3 }, tf);
-            var dict_comb = CreateCombDict(dict);
-*/
-            /*foreach(var k in dict)
-            {
-                foreach(var t in k.Value)
-                {
-                    Console.WriteLine($"q{k.Key} {t.Key} -> q{t.Value[0]} {(t.Value[1] == '1' ? 'b' : 'a')} {t.Value[2]}");
-                }
-            }*/
-            var text = "aba^";
-            (int nc, int np) = (1, 0);
-
-            var length = 5;
-
-            mch.PrintComb();
-            mch.RunMachine(nc, text, np);
-
-            /*(nc, np, text) = OneStep(nc, text, np, dict, 2);
-            Console.Write($"text : {text} ; ");
-            Console.Write($"cond : {nc}; ");
-            Console.WriteLine($"pos : {np}");*/
-
-            /* for(var i =0; i < 5; i++)
-             {
-                 ( nc, np, text) = OneStep(nc, text, np, dict, 2);
-                 Console.Write($"text : {text} ; ");
-                 Console.Write($"cond : {nc}; ");
-                 Console.WriteLine($"pos : {np}");
-             }*/
-
-           /* foreach(var a in dict_comb)
-            {
-                foreach(var e in a.Key)
-                {
-                    Console.Write($" {e}");
-                }
-                Console.WriteLine(a.Value);
-            }*/
-
         }
 
        
-
         
-        
-
     }
 }
